39feea618b07904237f7c2727cb5856e2da9dd86
Izzath <izzath0@gmail.com>
2020-04-30 19:52:02 +0530

    build files

---
+++ build/precache-manifest.f242f4d81b448b2cf7812e6a0e85acbe.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e0ca1b0a40f9ec2649271e360952db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f86b7f74fa6c552205f",
+    "url": "/static/js/main.501e5910.chunk.js"
+  },
+  {
+    "revision": "544b2f0bd80147cd4e1d",
+    "url": "/static/js/runtime-main.acf400f9.js"
+  }
+]);
---
+++ build/static/js/main.501e5910.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcorona=this.webpackJsonpcorona||[]).push([[0],[function(o,n,c){o.exports=c(1)},function(o,n){}],[[0,1]]]);
+//# sourceMappingURL=main.501e5910.chunk.js.map
